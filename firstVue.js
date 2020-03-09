e9b0efda3b0d538d4d3a88d78a6116dc33571e1a
Wade <wsw0615@gmail.com>
2020-03-09 17:07:20 +0800

    feat: to vedio #4

--- firstVue.js
+++ firstVue.js
@@ -1,2 +1,16 @@
-a = 1;
-console.log(a);
+new Vue({
+    el: '#vue-app',
+    data: {
+        name: 'Wade',
+        job: 'Assassin',
+        website: 'https://wade0615.github.io/',
+        websiteTag: '<a href="https://wade0615.github.io/">I use websitTag.</a>',
+        country: 'Taiwan'
+    },
+    methods: {
+        greet: function(time){
+            // return 'Good' + '' + time;
+            return `Good ${time} ${this.name}`;
+        }
+    }
+})
