26fa3b68ba205ca5663463fa8ed4b879689ed48c
Wade <wsw0615@gmail.com>
2020-03-11 19:46:17 +0800

    feat: #12 Looping with v-for

--- firstVue3.js
+++ firstVue3.js
@@ -2,7 +2,14 @@ new Vue({
     el: '#vue-app',
     data: {
         error: false,
-        success: false
+        success: false,
+        name: 'Jack',
+        characters: ['Mario','Luigi','Yoshi','Bowser'],
+        ninjas: [
+            {name: 'Ryu',age: 25},
+            {name: 'Yoshi',age: 35},
+            {name: 'Ken',age: 55}
+        ]
     },
     methods: {
 
