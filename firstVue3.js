739a3200cb68efae0c20c8823c6bdafe6b48b797
Wade <wsw0615@gmail.com>
2020-03-11 20:23:27 +0800

    feat: #13 Simple Punchbag Game

--- firstVue3.js
+++ firstVue3.js
@@ -9,10 +9,19 @@ new Vue({
             {name: 'Ryu',age: 25},
             {name: 'Yoshi',age: 35},
             {name: 'Ken',age: 55}
-        ]
+        ],
+        health: 100,
+        ended: false
     },
     methods: {
-
+        punch: function() {
+            this.health -= 10;
+            this.health <= 0 ? this.ended = true : ''; 
+        },
+        restart: function() {
+            this.health = 100;
+            this.ended = false;
+        }
     },
     computed: {
 
